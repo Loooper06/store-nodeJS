cacaf198f2dd87c2d77fbd9039150d729042e90b
Ali Golizadeh <79868303+Loooper06@users.noreply.github.com>
2022-11-04 13:44:44 +0330

    Remove course & product from Basket Grapgql

--- app/graphQL/mutation/basket.resolver.js
+++ app/graphQL/mutation/basket.resolver.js
@@ -3,10 +3,11 @@ const { StatusCodes: HttpStatus } = require("http-status-codes");
 const {
   VerifyAccessTokenInGraphQL,
 } = require("../../http/middlewares/verifyAccessToken");
-const { GraphQLString, GraphQLInt } = require("graphql");
-const { checkExistProduct } = require("../utils");
+const { GraphQLString } = require("graphql");
+const { checkExistProduct, checkExistCourse } = require("../utils");
 const { UserModel } = require("../../models/users");
 const { copyObject } = require("../../utils/functions");
+const createHttpError = require("http-errors");
 
 const AddProductToBasket = {
   type: ResponseType,
@@ -58,6 +59,31 @@ const AddCourseToBasket = {
     const user = await VerifyAccessTokenInGraphQL(req);
     const { courseID } = args;
     await checkExistCourse(courseID);
+    const course = await findCourseInBasket(user._id, courseID);
+    if (course) {
+      return {
+        statusCode: HttpStatus.CONFLICT,
+        data: {
+          message: "دوره قبلا به سبد خرید افزوده شده است",
+        },
+      };
+    } else {
+      await UserModel.updateOne(
+        { _id: user._id },
+        {
+          $push: {
+            "basket.courses": { courseID },
+          },
+        }
+      );
+    }
+
+    return {
+      statusCode: HttpStatus.OK,
+      data: {
+        message: "دوره با موفقیت به سبد خرید اضافه شد",
+      },
+    };
   },
 };
 
@@ -71,6 +97,47 @@ const RemoveProductToBasket = {
     const user = await VerifyAccessTokenInGraphQL(req);
     const { productID } = args;
     await checkExistProduct(productID);
+    const product = await findProductInBasket(user._id, productID);
+    let message;
+    if (!product)
+      throw createHttpError.NotFound("محصول مورد نظر در سبد خرید یافت نشد");
+
+    if (product.count > 1) {
+      await UserModel.updateOne(
+        {
+          _id: user._id,
+          "basket.products.productID": productID,
+        },
+        {
+          $inc: {
+            "basket.products.$.count": -1,
+          },
+        }
+      );
+      message = "تعداد محصول یک واحد کاهش یافت";
+    } else {
+      await UserModel.updateOne(
+        {
+          _id: user._id,
+          "basket.products.productID": productID,
+        },
+        {
+          $pull: {
+            "basket.products": {
+              productID,
+            },
+          },
+        }
+      );
+      message = "محصول با موفقیت از سبد خرید حذف شد";
+    }
+
+    return {
+      statusCode: HttpStatus.OK,
+      data: {
+        message,
+      },
+    };
   },
 };
 
@@ -84,6 +151,20 @@ const RemoveCourseToBasket = {
     const user = await VerifyAccessTokenInGraphQL(req);
     const { courseID } = args;
     await checkExistCourse(courseID);
+    const course = await findCourseInBasket(user._id, courseID);
+
+    if (!course) throw createHttpError.NotFound("دوره مورد نظر یافت نشد");
+    await UserModel.updateOne(
+      { _id: user._id, "basket.courses.courseID": courseID },
+      { $pull: { "basket.courses": { courseID } } }
+    );
+
+    return {
+      statusCode: HttpStatus.OK,
+      data: {
+        message: "دوره با موفقیت از سبد خرید حذف شد",
+      },
+    };
   },
 };
 
@@ -95,6 +176,15 @@ async function findProductInBasket(userID, productID) {
   );
   const userDetail = copyObject(findResult);
   return userDetail?.basket?.products?.[0];
+}
+
+async function findCourseInBasket(userID, courseID) {
+  const findResult = await UserModel.findOne(
+    { _id: userID, "basket.courses.courseID": courseID },
+    { "basket.courses.$": 1 }
+  );
+  const userDetail = copyObject(findResult);
+  return userDetail?.basket?.courses?.[0];
 }
 
 module.exports = {
